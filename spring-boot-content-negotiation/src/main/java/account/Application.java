6a6efeeb2c5b6501b8bed89269170cac795f647a
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2015-11-23 22:53:20 -0500

    Update AccountController

--- spring-boot-content-negotiation/src/main/java/account/Application.java
+++ spring-boot-content-negotiation/src/main/java/account/Application.java
@@ -1,7 +1,14 @@
 package account;
 
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +16,19 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Configuration
+    static class WebConfig extends WebMvcConfigurerAdapter {
+
+        @Autowired
+        private ThymeleafViewResolver thymeleafViewResolver;
+
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            registry.viewResolver(this.thymeleafViewResolver);
+            registry.enableContentNegotiation(new MappingJackson2JsonView());
+        }
+    }
+
 }
--- spring-boot-content-negotiation/src/main/java/account/web/AccountController.java
+++ spring-boot-content-negotiation/src/main/java/account/web/AccountController.java
@@ -1,22 +1,14 @@
 package account.web;
 
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import account.domain.Account;
 import account.domain.Customer;
 import account.service.AccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 class AccountController {
@@ -24,55 +16,26 @@ class AccountController {
     @Autowired
     protected AccountService accountService;
 
-    /**
-     * METHOD A: RESTful method
-     * TEST: 
-     * curl -i -H 'Accept: application/json' http://localhost:8080/accounts?name=TheCustomerName
-     */
-    @RequestMapping(value="/accounts", produces="application/json")
-    @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody List<Account> listWithMarshalling(Customer customer) {
-        return accountService.findAllAccounts(customer.getName());
+    
+    // curl -i -H 'Accept: application/json' http://localhost:8080/accounts?name=TheCustomerName
+    // curl -i http://localhost:8080/accounts?name=TheCustomerName
+    // curl -i -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"name":"TheCustomerName"}' http://localhost:8080/accounts\
+    // curl -i -H 'Content-Type: application/json' -d '{"name":"TheCustomerName"}' http://localhost:8080/accounts
+
+
+    @RequestMapping(value="/accounts")
+    public String listWithRequestParams(Customer customer, Model model) {
+        return listInternal(customer, model);
     }
 
-    /**
-     * METHOD B: View-based method
-     * TEST: 
-     * curl -i http://localhost:8080/accounts?name=TheCustomerName
-     */
-    @RequestMapping("/accounts")
-    public String listWithView(Model model, Customer customer) {
-
-        // Call RESTful method to avoid repeating account lookup logic
-        model.addAttribute( listWithMarshalling(customer) );
-
-        // Return the view to use for rendering the response
-        return "accounts/list";
-    }
-
-    /**
-     * METHOD C: RESTful method
-     * TEST: 
-     * curl -i -H 'Content-Type: application/json' -H 'Accept: application/json' -d '{"name":"TheCustomerName"}' http://localhost:8080/accounts
-     */
-    @RequestMapping(value="/accounts", produces="application/json", consumes="application/json")
-    @ResponseStatus(HttpStatus.OK)
-    public @ResponseBody List<Account> listWithMarshalling(@Valid @RequestBody Customer customer, BindingResult errors) {
-        return accountService.findAllAccounts(customer.getName());
-    }
-
-    /**
-     * METHOD D: View-based method
-     * TEST: 
-     * curl -i -H 'Content-Type: application/json' -d '{"name":"TheCustomerName"}' http://localhost:8080/accounts
-     */
     @RequestMapping(value="/accounts", consumes="application/json")
-    public String listWithView(Model model, @Valid @RequestBody Customer customer, BindingResult errors) {
+    public String listWithRequestBody(@RequestBody Customer customer, Model model) {
+        return listInternal(customer, model);
+    }
 
-        // Call RESTful method to avoid repeating account lookup logic
-        model.addAttribute( listWithMarshalling(customer) );
-
-        // Return the view to use for rendering the response
+    private String listInternal(Customer customer, Model model) {
+        model.addAttribute("accounts", accountService.findAllAccounts(customer.getName()) );
         return "accounts/list";
     }
+
 }
